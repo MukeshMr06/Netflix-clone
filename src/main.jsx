697b5a5a10d5cfc6a26c444f22afc26325e91dde
MukeshMr06 <mukeshmr0602@gmail.com>
2025-08-04 10:46:15 -0500

    upadate the browser

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter> {/* 👈 Fix here */}
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </HashRouter>
+  </React.StrictMode>
 )
